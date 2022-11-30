a3f75c6b8b3c2f25b1a3526c6f04cd274e4ada52
Sidharth D <zurajanaikatsurada1222@gmail.com>
2022-11-30 17:37:50 +0530

    Add files via upload

---
+++ 2 Pass Assembler program/pass1.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main(){
+	int locctr, sa, lctrt, er = 0, temp;
+	char label[10], mnemonic[10], operand[30], symbolst[10], mnemonict[10], opcode[3];
+	FILE* intmedf, * optabf, * symtabf, * inputf, * psizef;
+	inputf = fopen("input.txt", "r");
+	intmedf = fopen("intmed.txt", "a");
+	fscanf(inputf, "%s %s %s", label, mnemonic, operand);
+	if (strcmp(mnemonic, "START") == 0) {
+		sa = strtol(operand, NULL, 16);
+		locctr = sa;
+		fprintf(intmedf, "%x\t%s\t%s\t%s\n", locctr, label, mnemonic, operand);
+		fscanf(inputf, "%s %s %s", label, mnemonic, operand);
+	}
+	else {
+		locctr = 0X0000;
+	}
+	while (strcmp(mnemonic, "END") != 0) {
+		fprintf(intmedf, "%x\t", locctr);
+		if (strcmp(label, "-") != 0) {
+			symtabf = fopen("symtab.txt", "a+");
+			while ((fscanf(symtabf, "%x %s", lctrt, symbolst)) != EOF) {
+				if (strcmp(symbolst, label) == 0) {
+					er++;
+				}
+			}
+			if (er!=0){
+				printf("Error duplicate symbol\n");
+			}
+			else {
+				fprintf(symtabf, "%x\t%s\n", locctr, label);
+			}
+		}
+		er = 0;
+		optabf = fopen("optab.txt", "r");
+		while ((fscanf(optabf, "%s %s", mnemonict, opcode)) != EOF) {
+			if (strcmp(mnemonict, mnemonic) == 0) {
+				er++;
+			}
+		}
+		if (er != 0) {
+			locctr += 3;
+		}
+		else if (strcmp(mnemonic, "WORD") == 0) {
+			locctr += 3;
+		}
+		else if (strcmp(mnemonic, "RESW") == 0) {
+			locctr += 3 * strtol(operand, NULL, 16);
+		}
+		else if (strcmp(mnemonic, "RESB") == 0) {
+			locctr += strtol(operand, NULL, 16);
+		}
+		else if (strcmp(mnemonic, "BYTE") == 0) {
+			if (operand[0] == 'C') {
+				temp = strlen(operand) - 2;
+			}
+			else if (operand[0] == 'X') {
+				temp = (strlen(operand) - 2) / 2;
+			}
+			locctr += temp;
+		}
+		else {
+			printf("Error invalid operation code\n");
+		}
+		er = 0;
+		fprintf(intmedf, "%s\t%s\t%s\n", label, mnemonic, operand);
+		fscanf(inputf, "%s %s %s", label, mnemonic, operand);
+	}
+	fprintf(intmedf, "%x\t%s\t%s\t%s\n", locctr, label, mnemonic, operand);
+	fclose(symtabf);
+	fclose(intmedf);
+	psizef = fopen("psize.txt", "w");
+	printf("The program length is %d\n", (locctr - sa));
+	fprintf(psizef, "%x", (locctr - sa));
+	fclose(psizef);
+}
---
+++ 2 Pass Assembler program/pass2.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void main() {
+	int ctr1 = 0, ctr2 = 0, source_int, txtp=0, count=0;
+	FILE* intmedf, * psizef, * symtabf, * optabf, * outputf;
+	char locctr[10], label[10], mnemonic[10], operand[30], name[6]="      ", psize[5], objcode[100], 
+		opcode[10],mno[10], lctrt[10], symbolst[10], opaddr[10]="0000", target_string[3], sa[10];
+	char txtrec[30][60];
+	intmedf = fopen("intmed.txt", "r");
+	outputf = fopen("output.txt", "a");
+	psizef = fopen("psize.txt", "r");
+	optabf = fopen("optab.txt", "r");
+	fscanf(intmedf, "%s %s %s %s", locctr, label, mnemonic, operand);
+	strcpy(sa, locctr);
+	if (strcmp("START", mnemonic) == 0) {
+		strcpy(name, label);
+	}
+	fscanf(psizef, "%s", psize);
+	fclose(psizef);
+	fprintf(outputf,"H^%6s^%06s^%06s\n", name, locctr, psize);
+	fscanf(intmedf, "%s %s %s %s", locctr, label, mnemonic, operand);
+	printf("Object code:\n");
+	while (strcmp("END", mnemonic) != 0) {
+		ctr1 = 0; ctr2 = 0;
+		optabf = fopen("optab.txt", "r");
+		while ((fscanf(optabf, "%s %s", opcode,mno)) != EOF) {
+			if (strcmp(opcode, mnemonic) == 0) {
+				ctr1++;
+				break;
+			}
+		}
+		fclose(optabf);
+		if (ctr1 > 0) {
+			if (strcmp(operand, "-") != 0) {
+				symtabf = fopen("symtab.txt", "r");
+				while ((fscanf(symtabf, "%s %s", lctrt, symbolst)) != EOF) {
+					if (strcmp(operand, symbolst) == 0) {
+						ctr2++;
+						break;
+					}
+				}
+				fclose(symtabf);
+				if (ctr2 > 0) {
+					strcpy(opaddr, lctrt);
+				}
+				else {
+					printf("Error flag: Undefined symbol\n");
+				}
+			}
+			strcat(mno, opaddr);
+			strcpy(objcode, mno);
+		}
+		else if (strcmp(mnemonic, "BYTE") == 0) {
+			strcpy(objcode, "");
+			for (int i = 2; i < strlen(operand); i++) {
+				source_int = operand[i];
+				sprintf(target_string, "%x", source_int);
+				strcat(objcode, target_string);
+			}
+		}
+		else if (strcmp(mnemonic, "WORD") == 0) {
+			strcpy(objcode, operand);
+		}
+		else {
+			strcpy(objcode, "");
+		}
+		printf("%06s\n", objcode);
+		count = 0;
+		for (int i = 0; i < txtp; i++) {
+			for (int j = 0; j < strlen(txtrec[i]); j++) {
+				count++;
+			}
+		}
+		if ( count + strlen(objcode) +1 > 60) {
+			printf("hello");
+			fprintf(outputf, "T^%06s^%02x", txtrec[0], txtp);
+			for (int i = 0; i < txtp; i++) {
+				fprintf(outputf,"^%06s", txtrec[i]);
+			}
+			fprintf(outputf, "\n");
+			txtp = 0;
+			memset(txtrec, 0, 30 * 60 * sizeof txtrec[0][0]);
+		}
+		if (strcmp(objcode, "") != 0) {
+			strcpy(txtrec[txtp], objcode);
+			txtp++;
+		}
+		fscanf(intmedf, "%s %s %s %s", locctr, label, mnemonic, operand);
+	}
+	fprintf(outputf, "T^%06s^%02x", txtrec[0], txtp);
+	for (int i = 0; i < txtp; i++) {
+		fprintf(outputf, "^%06s", txtrec[i]);
+	}
+	fprintf(outputf, "\n");
+	fprintf(outputf, "E^%06s\n",sa );
+	fclose(outputf);
+	printf("Pass 2 complete\nCheck output.txt file\n");
+}
