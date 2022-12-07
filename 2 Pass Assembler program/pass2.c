94c5dcda119622a0ea39144f051bc9b90e52fb64
Sidharth D <zurajanaikatsurada1222@gmail.com>
2022-12-07 18:02:18 +0530

    fixed problem with starting obj address

--- 2 Pass Assembler program/pass2.c
+++ 2 Pass Assembler program/pass2.c
@@ -6,7 +6,7 @@ void main() {
 	int ctr1 = 0, ctr2 = 0, source_int, txtp=0, count=0;
 	FILE* intmedf, * psizef, * symtabf, * optabf, * outputf;
 	char locctr[10], label[10], mnemonic[10], operand[30], name[6]="      ", psize[5], objcode[100], 
-		opcode[10],mno[10], lctrt[10], symbolst[10], opaddr[10]="0000", target_string[3], sa[10];
+		opcode[10],mno[10], lctrt[10], symbolst[10], opaddr[10]="0000", target_string[3], sa[10], sao[10];
 	char txtrec[30][60];
 	intmedf = fopen("intmed.txt", "r");
 	outputf = fopen("output.txt", "a");
@@ -14,18 +14,18 @@ void main() {
 	optabf = fopen("optab.txt", "r");
 	fscanf(intmedf, "%s %s %s %s", locctr, label, mnemonic, operand);
 	strcpy(sa, locctr);
+	strcpy(sao, locctr);
 	if (strcmp("START", mnemonic) == 0) {
 		strcpy(name, label);
 	}
 	fscanf(psizef, "%s", psize);
 	fclose(psizef);
-	fprintf(outputf,"H^%6s^%06s^%06s\n", name, locctr, psize);
+	fprintf(outputf,"H^%6s^%06s^%06s\n", name, sao, psize);
 	fscanf(intmedf, "%s %s %s %s", locctr, label, mnemonic, operand);
-	printf("Object code:\n");
 	while (strcmp("END", mnemonic) != 0) {
 		ctr1 = 0; ctr2 = 0;
 		optabf = fopen("optab.txt", "r");
-		while ((fscanf(optabf, "%s %s", opcode,mno)) != EOF) {
+		while ((fscanf(optabf, "%s %s", opcode, mno)) != EOF) {
 			if (strcmp(opcode, mnemonic) == 0) {
 				ctr1++;
 				break;
@@ -66,7 +66,6 @@ void main() {
 		else {
 			strcpy(objcode, "");
 		}
-		printf("%06s\n", objcode);
 		count = 0;
 		for (int i = 0; i < txtp; i++) {
 			if (strlen(txtrec[i]) > 6 || strcmp(mnemonic, "BYTE") == 0) {
@@ -78,14 +77,15 @@ void main() {
 				count += 6;
 			}
 		}
-		if ( count + strlen(objcode) +1 > 60) {
-			printf("hello");
-			fprintf(outputf, "T^%06s^%02x", txtrec[0], txtp);
+		if (count + strlen(objcode) + 1 > 60) {
+			fprintf(outputf, "T^%06s^%02x", sao, count / 2);
+			printf("Count value is %d;\n", count);
 			for (int i = 0; i < txtp; i++) {
-				fprintf(outputf,"^%06s", txtrec[i]);
+				fprintf(outputf, "^%06s", txtrec[i]);
 			}
 			fprintf(outputf, "\n");
 			txtp = 0;
+			strcpy(sao, locctr);
 			memset(txtrec, 0, 30 * 60 * sizeof txtrec[0][0]);
 		}
 		if (strcmp(objcode, "") != 0) {
@@ -94,7 +94,7 @@ void main() {
 		}
 		fscanf(intmedf, "%s %s %s %s", locctr, label, mnemonic, operand);
 	}
-	fprintf(outputf, "T^%06s^%02x", txtrec[0], txtp);
+	fprintf(outputf, "T^%06s^%02x", sao, count/2);
 	for (int i = 0; i < txtp; i++) {
 		fprintf(outputf, "^%06s", txtrec[i]);
 	}
